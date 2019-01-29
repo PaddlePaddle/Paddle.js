d40666bb16cccdcb41274c0af07919180ccd14b3
yangmingming <yangmingming@baidu.com>
2019-01-29 20:27:33 +0800

    conv 适配变量
    Change-Id: I8c8b1555de412758751b6e10d4123545d2cfc284

--- src/shader/f_elementwise_conv2d3_shader.c
+++ src/shader/f_elementwise_conv2d3_shader.c
@@ -6,20 +6,33 @@
     precision mediump int;
 #endif
 // canvas默认宽高
-const int size = DIM_SIZE;
+const int size_w = DIM_SIZE_WIDTH;
+const int size_h = DIM_SIZE_HEIGHT;
 
-const int F_LENGTH = FILTER_SIZE;
-const int O_LEGNTH = ORIGIN_SIZE;
-const int outLength = OUT_SIZE;
-// 步长
-int stride = STRIDE;
+// 拆分filter的宽高，不一定相等
+const int filter_w = FILTER_SIZE_WIDTH;
+const int filter_h = FILTER_SIZE_HEIGHT;
+
+// 拆分输入张量
+const int origin_w = ORIGIN_SIZE_WIDTH;
+const int origin_h = ORIGIN_SIZE_HEIGHT;
+
+// 拆分输出张量的宽高，不一定相等
+const int out_w = OUT_SIZE_WIDTH;
+const int out_h = OUT_SIZE_HEIGHT;
+
+// 拆分步长
+const int stride_h = STRIDE_HORIZONTAL;
+const int stride_v = STRIDE_VERTICAL;
+
 // padding的数目
-int padLeft = PAD_LEFT;
-int padTop = PAD_TOP;
+const int padLeft = PAD_LEFT;
+const int padTop = PAD_TOP;
+
 // dilation膨胀系数
 int dilation = DILATION;
 
-uniform float filter[F_LENGTH * F_LENGTH];
+uniform float filter[filter_w * filter_h];
 uniform sampler2D origin;
 
 varying vec2 vCoord;
@@ -27,8 +40,8 @@ varying vec2 sCoord;
 
 vec2 transToOut(vec2 vcoord) {
     vec2 v2;
-    v2.x = vcoord.x * float(size);
-    v2.y = vcoord.y * float(size);
+    v2.x = vcoord.x * float(size_w);
+    v2.y = vcoord.y * float(size_h);
     return v2;
 }
 
@@ -45,20 +58,20 @@ void main(void) {
     vec2 outCoord = transToOut(v2);
     // 输出数据
     vec4 v4;
-    if (outCoord.x < float(outLength) && outCoord.y < float(outLength)) {
+    if (outCoord.x < float(out_w) && outCoord.y < float(out_h)) {
         float result = 0.0;
         // X、Y方向的移动步长
         int disX = -padLeft;
         int disY = -padTop;
         vec2 oriCoord;
-        for (int fy = 0; fy < F_LENGTH; fy++) {
-            float oy = floor(outCoord.y) * float(stride) + float(fy * dilation + disY);
-            for (int fx = 0; fx < F_LENGTH; fx++) {
-                float ox = floor(outCoord.x) * float(stride) + float(fx * dilation + disX);
-                if (oy >= 0.0 && oy < float(O_LEGNTH) && ox >= 0.0 && ox < float(O_LEGNTH)) {
-                    oriCoord.x = ox / float(O_LEGNTH);
-                    oriCoord.y = oy / float(O_LEGNTH);
-                    result += filter[int(F_LENGTH) * fy + fx] * texture2D(origin, oriCoord).r;
+        for (int fy = 0; fy < filter_h; fy++) {
+            float oy = floor(outCoord.y) * float(stride_v) + float(fy * dilation + disY);
+            for (int fx = 0; fx < filter_w; fx++) {
+                float ox = floor(outCoord.x) * float(stride_h) + float(fx * dilation + disX);
+                if (oy >= 0.0 && oy < float(origin_h) && ox >= 0.0 && ox < float(origin_w)) {
+                    oriCoord.x = ox / float(origin_w);
+                    oriCoord.y = oy / float(origin_h);
+                    result += filter[int(filter_w) * fy + fx] * texture2D(origin, oriCoord).r;
                 }
             }
         }

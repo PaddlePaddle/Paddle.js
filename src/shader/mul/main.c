31624759865e6a8e544c475c6b6c4c2a0008ff52
zhangmiao06 <zhangmiao06@baidu.com>
2019-03-10 16:58:07 +0800

    初版mul

--- src/shader/mul/main.c
+++ src/shader/mul/main.c
@@ -4,23 +4,23 @@ void main(void) {
     v2.x = vCoord.x;
     v2.y = vCoord.y;
     // 获取原始长度
-    vec2 outCoord = moveTexture2PosToReal_texture_out(v2);
+    vec2 out_coord = moveTexture2PosToReal_texture_out(v2);
     // 输出数据
     vec4 v4;
     for (int i = 0; i < 4; i++) {
-        int n = getArrayIndexFromTexturePos_texture_out(vec3(outCoord.x, outCoord.y, float(i)));
+        int n = getArrayIndexFromTexturePos_texture_out(vec3(out_coord.x, out_coord.y, float(i)));
         // 获取output的坐标
-        ivec4 outPos = getTensorPosFromArrayIndex_out(n);
+        ivec4 out_pos = getTensorPosFromArrayIndex_out(n);
         for (int j = 0; j < width_shape_origin; j++) {
             // filter数据
-            int fIndex = getArrayIndexFromTensorPos_filter(ivec4(outPos[0], outPos[1], j, outPos[3]));
-            vec3 fPos = getTexturePosFromArrayIndex_texture_filter(fIndex);
+            int f_index = getArrayIndexFromTensorPos_filter(ivec4(out_pos[0], out_pos[1], j, out_pos[3]));
+            vec3 f_pos = getTexturePosFromArrayIndex_texture_filter(f_index);
             // origin数据
-            int oIndex = getArrayIndexFromTensorPos_origin(ivec4(outPos[0], outPos[1], outPos[2], j));
-            vec3 oPos = getTexturePosFromArrayIndex_texture_origin(oIndex);
+            int o_ndex = getArrayIndexFromTensorPos_origin(ivec4(out_pos[0], out_pos[1], out_pos[2], j));
+            vec3 o_pos = getTexturePosFromArrayIndex_texture_origin(o_ndex);
             
-            v4[i] += (getValueFromTexturePos_texture_filter(fPos) *
-            getValueFromTexturePos_texture_origin(oPos));
+            v4[i] += (getValueFromTexturePos_texture_filter(f_pos) *
+            getValueFromTexturePos_texture_origin(o_pos));
         }
     }
     gl_FragColor = v4;

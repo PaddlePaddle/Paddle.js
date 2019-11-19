73ac6be64e625ddf1f1ede5865cfa5ff01753c35
zhangmiao06 <zhangmiao06@baidu.com>
2019-11-19 10:38:36 +0800

    用于人脸娱乐化打包

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                     plugins: ['@babel/transform-runtime']
                 }
             }
-        },{
+        }, {
             test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
             loader: 'url-loader?limit=30000&name=[name].[ext]'
         }, {

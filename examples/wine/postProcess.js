9c5a6673d3f7af1c4a54969d87f31d566ca48e5e
zhangjingyuan02 <zhangjingyuan02@baidu.com>
2020-11-11 11:34:49 +0800

    refactor: del old structure

--- examples/wine/postProcess.js
+++
@@ -1,8 +0,0 @@
-// index.js
-import Map from '../../test/data/wine.map.json';
-import Utils from '../../src/utils/utils';
-
-export default function postProcess(data) {
-    let maxItem = Utils.getMaxItem(data);
-    return Map[maxItem.index];
-}

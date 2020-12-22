32a4f430af0d3730ee610e1a65865743d3ac4bca
sukkee <382248373@qq.com>
2020-12-22 21:42:45 +0800

    Update backend.ts

--- packages/paddlejs-core/src/backend.ts
+++ packages/paddlejs-core/src/backend.ts
@@ -6,7 +6,7 @@
 import { OpData, ModelVar as FetchInfo } from './commons/interface';
 
 export default abstract class PaddlejsBackend {
-    public abstract init(): void;
+    abstract init(): void;
 
     abstract createProgram(opts: object): string;
 

386679bcc6c23f0b777f57f592a7a3986bce03f3
JingyuanZhang <382248373@qq.com>
2021-01-06 19:14:08 +0800

    feat(models): add package paddlejs-models and mobilenet model

---
+++ packages/paddlejs-models/mobilenet/webpack.dev.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './demo/index.ts'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        host: '0.0.0.0',
+        port: 8866
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            chunks: ['index'],
+            template: './demo/index.html'
+        })
+    ],
+    resolve: {
+        // Add ".ts" and ".tsx" as resolvable extensions.
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};

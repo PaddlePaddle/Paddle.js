bb6f03ecf6450b1b5640c8dfdb1c425896dea926
changy1105 <18392600593@163.com>
2021-01-07 12:02:48 +0800

    feat(examples): mobileNet and wine

--- packages/paddlejs-examples/mobilenet/index.ts
+++ packages/paddlejs-examples/mobilenet/index.ts
@@ -1,45 +1,29 @@
-import { Runner } from '@paddlejs/paddlejs-core';
-import registerWebGLBackend from '@paddlejs/paddlejs-backend-webgl';
+import * as mobilenet from '@paddlejs-models/mobilenet';
 import map from './data/map.json';
-
-registerWebGLBackend();
 
 declare let window: Window & {
     statistic: any
 };
 window.statistic = {};
-let runner;
 
 load();
 
 async function load() {
-    const path = 'https://paddlejs.cdn.bcebos.com/models/mobileNetV2/model.json';
-    runner = new Runner({
-        modelPath: path,
+    const path = 'https://paddlejs.cdn.bcebos.com/models/mobileNetV2';
+    await mobilenet.load({
+        path,
         fileCount: 4,
-        feedShape: {
-            fw: 224,
-            fh: 224
-        },
-        fill: '#fff',
-        inputType: 'image',
-        targetSize: {
-            height: 224,
-            width: 224
-        },
         mean: [0.485, 0.456, 0.406],
         std: [0.229, 0.224, 0.225]
-    });
-    await runner.init();
+    }, map);
     document.getElementById('loading')!.style.display = 'none';
 }
 
 async function run(input: HTMLElement) {
     window.statistic.startTime = (+new Date());
-    const res = await runner.predict(input);
+    const res = await mobilenet.classify(input);
     window.statistic.endTime = (+new Date()) - window.statistic.startTime;
-    const maxItem = getMaxItem(res);
-    document.getElementById('txt')!.innerHTML = map['' + maxItem.index];
+    document.getElementById('txt')!.innerHTML = res;
     document.getElementById('all-performance-time')!.innerHTML = '计算时间是' + window.statistic.endTime;
 }
 
@@ -66,11 +50,4 @@ function selectImage(file) {
         };
     };
     reader.readAsDataURL(file.files[0]);
-}
-
-// 获取数组中的最大值和索引
-function getMaxItem(datas: number[] = []) {
-    const max: number = Math.max.apply(null, datas);
-    const index: number = datas.indexOf(max);
-    return { value: max, index };
 }

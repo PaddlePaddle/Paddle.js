e3d9b497a2f7f92c7027c0437fe70172216e891a
changy1105 <18392600593@163.com>
2021-02-01 13:53:13 +0800

    feat(mediapipe): add camera

--- packages/paddlejs-mediapipe/camera/demo/index.ts
+++ packages/paddlejs-mediapipe/camera/demo/index.ts
@@ -1,4 +1,3 @@
-// import * as humanseg from '../../../paddlejs-models/humanseg/src/index';
 import Camera from '../src/index';
 
 let camera = null;
@@ -10,11 +9,11 @@ const startBtn = document.getElementById('start') as any;
 const endBtn = document.getElementById('end') as any;
 const shotBtn = document.getElementById('shot') as any;
 const canvas = document.getElementById('demo') as HTMLCanvasElement;
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 load();
 
-/* 循环跑模型 */
+/* canvas循环绘制 */
 let loopFlag = true;
 async function loop() {
     let input = camera.curVideo;
@@ -23,21 +22,20 @@ async function loop() {
     }
 }
 
-// 视频开始播放时，开始模型预热
-video && video.addEventListener('play', async function () {
+// 视频开始播放，loading消失
+video && video.addEventListener('loadeddata', async function () {
     startBtn.disabled = false;
     startBtn.innerText = '开始测试';
+    loadingDom && loadingDom.remove();
 });
 
-// 点击开始按钮，开始正式跑模型
+// 点击视频控制按钮，实现视频播放/截图/暂停功能
 videoToolDom.addEventListener('click', function(e: any) {
     if (e.target.id === 'start') {
+        camera.curVideo.play();
         shotBtn.disabled = false;
         endBtn.disabled = false;
-        camera.curVideo.play();
-        setTimeout(() => {
-            startBtn.disabled = true;
-        });
+        startBtn.disabled = true;
     }
     if (e.target.id === 'shot') {
         loopFlag = true;
@@ -77,13 +75,9 @@ async function load() {
     }).catch(e => {
         console.log(e);
     });
-    startBtn.disabled = false;
-    startBtn.innerText = '开始测试';
-    loadingDom && loadingDom.remove();
 }
 
 async function run(input) {
-    console.log(input);
     // canvas绘制video当前帧
     canvas.width = input.width;
     canvas.height = input.height;

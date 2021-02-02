c8a81fc846adeac7096e871dbf840fe7184676a0
changy1105 <18392600593@163.com>
2021-02-02 15:21:36 +0800

    feat(mediapipe): add camera

--- packages/paddlejs-mediapipe/camera/demo/index.ts
+++ packages/paddlejs-mediapipe/camera/demo/index.ts
@@ -4,83 +4,36 @@ let camera = null;
 let loadingDom = document.getElementById("isLoading");
 let video = document.getElementById('video') as any;
 const videoToolDom = document.getElementById('video-tool');
-const videoSelect = document.getElementById('videoSelect') as any;
 const startBtn = document.getElementById('start') as any;
-const endBtn = document.getElementById('end') as any;
-const shotBtn = document.getElementById('shot') as any;
+const pauseBtn = document.getElementById('pauseBtn') as any;
+const stopBtn = document.getElementById('stopBtn') as any;
 const canvas = document.getElementById('demo') as HTMLCanvasElement;
-const ctx = canvas.getContext('2d');
 
 load();
 
-/* canvas循环绘制 */
-let loopFlag = true;
-async function loop() {
-    let input = camera.curVideo;
-    if (loopFlag) {
-        await run(input);
-    }
-}
-
 // 视频开始播放，loading消失
 video && video.addEventListener('loadeddata', async function () {
-    startBtn.disabled = false;
-    startBtn.innerText = '开始测试';
     loadingDom && loadingDom.remove();
 });
 
 // 点击视频控制按钮，实现视频播放/截图/暂停功能
 videoToolDom.addEventListener('click', function(e: any) {
     if (e.target.id === 'start') {
-        camera.curVideo.play();
-        shotBtn.disabled = false;
-        endBtn.disabled = false;
-        startBtn.disabled = true;
+        camera.start();
     }
-    if (e.target.id === 'shot') {
-        loopFlag = true;
-        loop();
+    if (e.target.id === 'pause') {
+        camera.pause();
     }
-    if (e.target.id === 'end') {
-        loopFlag = false;
-        shotBtn.disabled = true;
-        endBtn.disabled = true;
-        startBtn.disabled = false;
-        camera.curVideo.pause();
+    if (e.target.id === 'stop') {
+        camera.stop();
     }
 });
 
 async function load() {
-    camera = new Camera({
-        // 用来显示摄像头图像的dom
-        videoDom: video
-    });
-    /* 获取摄像头并开始视频流 */
-    await camera.getDevices().then(devices => {
-        if (devices.length) {
-            camera.run(devices[0].deviceId);
-            devices.forEach((element, index) => {
-                let option = document.createElement('option');
-                option.value = element.deviceId;
-                option.text = (index + 1);
-                videoSelect.appendChild(option);
-            });
-            videoSelect.onchange = () => {
-                camera.run(videoSelect.value);
-            };
+    camera = new Camera(video, {
+        onFrame: canvas => {
+            console.log(canvas, 'canvas')
         }
-        else {
-            camera.run();
-        }
-    }).catch(e => {
-        console.log(e);
     });
-}
-
-async function run(input) {
-    // canvas绘制video当前帧
-    canvas.width = input.width;
-    canvas.height = input.height;
-    ctx.drawImage(input, 0, 0, input.width, input.height);
 }
 
--- packages/paddlejs-mediapipe/camera/test/index.test.ts
+++ packages/paddlejs-mediapipe/camera/test/index.test.ts
@@ -1,198 +1,35 @@
 import Camera from '../src/index';
 
-let video = document.createElement('video') as any;
-let video1 = document.createElement('video') as any;
-let video2 = document.createElement('video') as any;
+let video = document.createElement('video');
 
-// video.srcObject = null;
-
-// 创建mock函数
-const mockFn1 = jest.fn().mockResolvedValue('');
-
-const mockFn2 = jest.fn();
-
-const mockFn3 = jest.fn();
-
-Object.defineProperty(navigator, 'mediaDevices', {
-    writable: true,
-    value: {
-        enumerateDevices: mockFn1,
-        getUserMedia: mockFn1
-    }
-});
-
-Object.defineProperty(navigator, 'userAgent', {
-    writable: true,
-    value: 'iphone'
-});
-
-const deviceInfos = [
-    {
-        deviceId: 'd7f3e03a5a257dbf6971d0dcbab6500a551dcbcc504e23a8b551bf1fbe810ce3',
-        groupId: '463296e5a120f9e9eed4377098a3f36f88fdee953c3e570dc162069ebd865b82',
-        kind: 'videoinput',
-        label: 'FaceTime高清摄像头（内建） (05ac:8514)'
-    }
-];
-
-const stream = {
-    active: true,
-    id: "WXtVeM6aEr9l1Gglb7rcJP9dWlMfaSGyiCQz",
-    onactive: null,
-    onaddtrack: null,
-    oninactive: null,
-    onremovetrack: null
-};
-
-let camera = new Camera({
-    // 用来显示摄像头图像的dom
-    videoDom: video,
-    constraints: {
-        video: true
-    }
-});
-
-let camera1 = new Camera({
-    // 用来显示摄像头图像的dom
-    videoDom: video1,
-    width: 200
-});
-
-let camera2 = new Camera({
-    // 用来显示摄像头图像的dom
-    videoDom: video2,
-    height: 300
-});
-
-let camera3 = new Camera({
-    // 用来显示摄像头图像的dom
-    videoDom: video,
-    constraints: {
-        video: true
-    },
-    deviceInfos
-});
-
-test('camera constructor', () => {
-    console.log(camera, 'camera');
-    const innerWidth = window.innerWidth;
-    expect(video.width).toBe(innerWidth);
-    expect(video1.width).toBe(200);
-    expect(video2.height).toBe(300);
-});
-
-test('called getDevices', () => {
-    /* 获取摄像头 */
-    camera3.getDevices();
-    expect(mockFn1).toHaveBeenCalled();
-});
-
-test('called run', () => {
-    window.alert = () => {};
-    camera3.run(deviceInfos[0].deviceId, mockFn2);
-    expect(mockFn1).toHaveBeenCalled();
-});
-
-test('called run not exist mediaDevices', () => {
-    Object.defineProperty(navigator, 'mediaDevices', {
-        writable: true,
-        value: undefined
-    });
-    camera3.run(deviceInfos[0].deviceId, mockFn2);
-    expect(mockFn1).toHaveBeenCalled();
-});
-
-test('called run exist webkitGetUserMedia', () => {
-    Object.defineProperty(navigator, 'webkitGetUserMedia', {
-        writable: true,
-        value: mockFn2
-    });
-    Object.defineProperty(navigator, 'mediaDevices', {
-        writable: true,
-        value: undefined
-    });
-    let camera = new Camera({
+test('camera constructor not exist navigator.getUserMedia', () => {
+    const mockFnNotSupported = jest.fn();
+    let camera = new Camera(
         // 用来显示摄像头图像的dom
-        videoDom: video,
-        constraints: {
-            video: true
-        },
-        deviceInfos
-    });
-    camera.run(deviceInfos[0].deviceId, mockFn2);
-    expect(mockFn2).toHaveBeenCalled();
+        video, {
+            width: 100,
+            height: 100,
+            onNotSupported: mockFnNotSupported
+        });
+    expect(video.width).toBe(100);
+    expect(mockFnNotSupported).toHaveBeenCalled();
 });
 
-test('called run exist mozGetUserMedia', () => {
-    Object.defineProperty(navigator, 'mozGetUserMedia', {
-        writable: true,
-        value: mockFn2
-    });
-    Object.defineProperty(navigator, 'mediaDevices', {
-        writable: true,
-        value: undefined
-    });
-    Object.defineProperty(navigator, 'webkitGetUserMedia', {
-        writable: true,
-        value: undefined
-    });
-    let camera = new Camera({
-        // 用来显示摄像头图像的dom
-        videoDom: video,
-        constraints: {
-            video: true
-        },
-        deviceInfos
-    });
-    camera.run(deviceInfos[0].deviceId, mockFn2);
-    expect(mockFn2).toHaveBeenCalled();
-});
-
-test('called run exist getUserMedia', () => {
+test('camera constructor exist navigator.getUserMedia', () => {
+    const mockFnGetUserMedia = jest.fn();
+    // @ts-ignore
+    window.URL = 'xxx';
     Object.defineProperty(navigator, 'getUserMedia', {
         writable: true,
-        value: mockFn2
+        value: mockFnGetUserMedia
     });
-    Object.defineProperty(navigator, 'mediaDevices', {
-        writable: true,
-        value: undefined
-    });
-    Object.defineProperty(navigator, 'webkitGetUserMedia', {
-        writable: true,
-        value: undefined
-    });
-    Object.defineProperty(navigator, 'mozGetUserMedia', {
-        writable: true,
-        value: undefined
-    });
-    let camera = new Camera({
+    let camera = new Camera(
         // 用来显示摄像头图像的dom
-        videoDom: video,
-        constraints: {
-            video: true
-        },
-        deviceInfos
-    });
-    camera.run(deviceInfos[0].deviceId, mockFn2);
-    expect(mockFn2).toHaveBeenCalled();
-});
-
-test('called success', () => {
-    video.srcObject = null;
-    video.play = () => {};
-    const documentLoadedEvent = new Event('loadeddata');
-
-    let camera = new Camera({
-        // 用来显示摄像头图像的dom
-        videoDom: video,
-        constraints: {
-            video: true
-        },
-        deviceInfos
-    });
-    camera.success(stream, mockFn3);
-    video.dispatchEvent(documentLoadedEvent);
-    expect(mockFn3).toHaveBeenCalledTimes(1);
+        video, {
+            width: 100,
+            height: 100
+        });
+    expect(mockFnGetUserMedia).toHaveBeenCalled();
 });
 
 

e2888a19a381ce49072af03f1ddbd983f7cd032e
JingyuanZhang <382248373@qq.com>
2021-02-19 02:00:34 +0800

    feat(nodegl): init nodegl backend

---
+++ packages/paddlejs-backend-nodegl/src/register.ts
@@ -0,0 +1,9 @@
+/**
+ * @file register nodegl backend
+ * @author zhangjingyuan
+ */
+
+import './nodegl';
+import glInstance from '../../paddlejs-backend-webgl/src/index';
+
+export default glInstance;

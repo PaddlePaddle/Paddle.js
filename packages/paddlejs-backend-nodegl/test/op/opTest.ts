e6fdccf40e42c035843c05911dfe153c7c117a48
JingyuanZhang <382248373@qq.com>
2021-02-19 18:14:30 +0800

    chore(nodegl): update nodegl scripts and add demo

---
+++ packages/paddlejs-backend-nodegl/test/op/opTest.ts
@@ -0,0 +1,26 @@
+import { Runner } from '../../../paddlejs-core/src/index';
+import backend from '../../src/index';
+
+const path = require('path');
+
+const opName = 'elementwise_add';
+const modelDir = path.resolve(__dirname, './data');
+const modelPath = `${modelDir}/${opName}.json`;
+
+async function run() {
+    const runner = new Runner({
+        modelPath,
+        fileCount: 0,
+        feedShape: {
+            fw: 3,
+            fh: 3
+        },
+        needPreheat: false
+    });
+    await runner.init();
+    const executeOP = runner.weightMap[0];
+    runner.executeOp(executeOP);
+    console.log(await backend.read());
+}
+
+run();

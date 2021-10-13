dbb20074cd69ca418f4d596c2a71cad3efc36aa1
JingyuanZhang <382248373@qq.com>
2021-10-13 17:10:33 +0800

    test(e2e): add gesture model e2e

---
+++ e2e/tests/gesture.test.ts
@@ -0,0 +1,24 @@
+describe('e2e test gesture model', () => {
+    const { paddlejs } = require('./global.d.ts');
+    const CUR_URL = 'http://localhost:9898/';
+
+    beforeAll(async () => {
+        await page.goto(CUR_URL);
+    });
+
+    it('gesture predict', async () => {
+        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+        const res = await page.evaluate(async () => {
+            const ok = document.querySelector('#ok');
+            const gesture = paddlejs['gesture'];
+
+            await gesture.load();
+            const res = await await gesture.classify(ok);
+
+            return res;
+        });
+
+        const expected = 'ok';
+        await expect(res.type).toEqual(expected);
+    });
+});

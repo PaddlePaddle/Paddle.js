ee8080218c62583210b1b6f9e3dbc6df18a58b8b
yueshuangyan <yueshuangyan@baidu.com>
2021-11-17 20:39:52 +0800

    feat(mediapipe): add data-processor

---
+++ packages/paddlejs-mediapipe/data-processor/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: ['./demo/index'],
+    devtool: 'inline-source-map',
+    devServer: {
+        host: 'localhost',
+        port: 9000
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            dangerouslyAllowCleanPatternsOutsideProject: true
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './demo/index.html'
+        })
+    ],
+    resolve: {
+        // Add ".ts" and ".tsx" as resolvable extensions.
+        extensions: ['.ts', '.js'],
+        fallback: {
+            path: require.resolve('path-browserify')
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};

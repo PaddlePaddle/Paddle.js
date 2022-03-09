f9f52f657c5c9fa0a781982b9c07505944d8799e
JingyuanZhang <382248373@qq.com>
2022-03-09 18:59:58 +0800

    feat(webgl): close gl query time unless  set env.performance true

--- packages/paddlejs-backend-webgl/src/utils/queryProcess.ts
+++ packages/paddlejs-backend-webgl/src/utils/queryProcess.ts
@@ -3,6 +3,8 @@
  * @author yueshuangyan
  */
 
+import { env } from '@paddlejs/paddlejs-core';
+
 export default {
     getQueryTime(gl, query) {
         const timeElapsedNanos = gl.getQueryParameter(query, gl.QUERY_RESULT);
@@ -10,7 +12,7 @@ export default {
         return timeElapsedNanos;
     },
     beginQuery(gl, glVersion) {
-        if (glVersion === 2) {
+        if (glVersion === 2 && env.get('performance')) {
             const ext = gl.getExtension('EXT_disjoint_timer_query_webgl2');
             if (!ext) {
                 return;
@@ -22,7 +24,7 @@ export default {
         return null;
     },
     endQuery(gl, glVersion, query) {
-        if (glVersion === 2) {
+        if (glVersion === 2 && env.get('performance')) {
             const ext = gl.getExtension('EXT_disjoint_timer_query_webgl2');
             if (!ext) {
                 return;
